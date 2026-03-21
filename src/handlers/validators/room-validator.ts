930e38f73eb83e91222d17aa22ddbf8b546db6b8
Julian <163280302+JEstermann@users.noreply.github.com>
2026-03-21 16:18:23 +0100

    Fix room capacity

--- src/handlers/validators/room-validator.ts
+++ src/handlers/validators/room-validator.ts
@@ -6,7 +6,7 @@ export const CreateRoomValidator = Joi.object({
     description: Joi.string().min(10).required(),
     images: Joi.array().items(Joi.string().uri()).min(1).required(),
     type: Joi.string().min(2).required(),
-    capacity: Joi.number().integer().min(50).max(100).required(),
+    capacity: Joi.number().integer().min(15).max(30).required(),
     isAccessible: Joi.boolean().optional(),
     isMaintenance: Joi.boolean().optional(),
 }).options({ abortEarly: false });
@@ -15,7 +15,7 @@ export const ListRoomValidator = Joi.object<ListRoomRequest>({
     page: Joi.number().min(1).optional(),
     size: Joi.number().min(1).max(100).optional(),
     name: Joi.string().min(3).max(255).optional(),
-    capacityMax: Joi.number().integer().min(50).max(100).optional(),
+    capacityMax: Joi.number().integer().min(15).max(30).optional(),
 }).options({abortEarly: false})
 
 export const RoomIdValidator = Joi.object<RoomIdRequest>({
@@ -28,7 +28,7 @@ export const UpdateRoomValidator = Joi.object<UpdateRoomRequest>({
     description: Joi.string().min(10).optional(),
     images: Joi.array().items(Joi.string().uri()).min(1).optional(),
     type: Joi.string().min(2).optional(),
-    capacity: Joi.number().integer().min(50).max(100).optional(),
+    capacity: Joi.number().integer().min(15).max(30).optional(),
     isAccessible: Joi.boolean().optional(),
     isMaintenance: Joi.boolean().optional(),
 })
