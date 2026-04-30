7ea5b9d63c8861638a0aba224e8d8fd038be0c98
Massi <aidelmassi@gmail.com>
2026-04-30 09:40:27 +0200

    Update .gitignore to include .env and refine database connection settings

--- src/database/database.ts
+++ src/database/database.ts
@@ -4,11 +4,11 @@ import { Room } from "./entities/room.js";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: process.env.DB_HOST || "localhost", 
+    host: process.env.DB_HOST as string, 
     port: parseInt(process.env.DB_PORT || "3306"),
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "cpZTMQ760?ZTLoB8j*5WW~1hT,4*H",
-    database: process.env.DB_DATABASE || "cinema_db",
+    username: process.env.DB_USERNAME as string,
+    password: process.env.DB_PASSWORD as string,
+    database: process.env.DB_DATABASE as string,
     
     synchronize: process.env.NODE_ENV === "development", 
     logging: process.env.NODE_ENV === "development",
