c3008efda16880b797ce92aa5ec8b627753f2791
Massi <aidelmassi@gmail.com>
2026-04-30 08:03:51 +0200

    Fix database password formatting and update DataSource configuration

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,15 +1,16 @@
-import "dotenv/config"; 
+import "dotenv/config";
 import { DataSource } from "typeorm";
 import { Room } from "./entities/room.js";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || "3306"), 
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    synchronize: process.env.NODE_ENV === "development", // True seulement si on est dév
+    host: process.env.DB_HOST || "localhost", 
+    port: parseInt(process.env.DB_PORT || "3306"),
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "cpZTMQ760?ZTLoB8j*5WW~1hT,4*H",
+    database: process.env.DB_DATABASE || "cinema_db",
+    
+    synchronize: process.env.NODE_ENV === "development", 
     logging: process.env.NODE_ENV === "development",
     entities: [Room]
 });
